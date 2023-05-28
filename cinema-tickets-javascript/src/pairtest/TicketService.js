d4a626f7bd43bb141f0eecb9e1178cc94058c0ed
Soheil Saifipour <s.saifipour@gmail.com>
2023-05-28 16:05:33 +0100

    Complete purchaseTickets
    * Check for invalid purchage request (number of tickets)
    * Check for invalid purchage request (at least one Adult ticket request)
    * Calculate total amount and number of seats
    * Make payment and reserve seats

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -25,4 +25,44 @@ export default class TicketService {
     this.seatReservationService = seatReservationService;    
   } 
 
+  /**
+ * Purchase ticket for customer
+ * @param {number} accountId The customer account Id.
+ * @param {TicketTypeRequest} ...ticketTypeRequests The ticket purchase requests.
+ */
+  purchaseTickets(accountId, ...ticketTypeRequests) {
+    // throws InvalidPurchaseException
+
+
+     // Check if ticket purchase request items count is in the valid range
+     let totalTickets = ticketTypeRequests.reduce((sum, request)=> sum + request.getNoOfTickets(), 0);  
+     if(totalTickets === 0 || totalTickets > this.#getMaxTicketPerPurchase()){
+       throw new InvalidPurchaseException( `Invalid ticket purchase request. Please ensure that you are purchasing between 1 and ${this.#getMaxTicketPerPurchase()} tickets.`);
+     }
+    
+     
+    // Check if at least one Adult ticket request should be present.
+    const existsAdultTicketRequest = ticketTypeRequests.some((request)=> request.getTicketType() === 'ADULT');
+    if(!existsAdultTicketRequest){
+      throw new InvalidPurchaseException( `Invalid ticket purchase request. At least one Adult ticket request should be present.`);
+    }   
+
+    // Calculate total order amount and total seat number
+    let totalOrderAmount = 0;
+    let totalSeats = 0;  
+
+    ticketTypeRequests.forEach(request => {        
+      // Calculate total order amount
+      var item = this.#getTicketsPriceList().find(item=>item.ticketType === request.getTicketType());
+      totalOrderAmount +=  item.price * request.getNoOfTickets();
+
+      // Do not include infants in seat reservation
+      if (request.getTicketType() !== 'INFANT') {
+        totalSeats += request.getNoOfTickets();      
+      }
+    });
+
+    this.seatReservationService.reserveSeats(accountId, totalSeats); 
+    this.paymentService.makePayment(accountId, totalOrderAmount);
+  }
 }
